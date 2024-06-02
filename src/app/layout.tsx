d0041e1d120f6a4f94fb73556c770485eb815eae
nktaonop <22onopnikita08@gmail.com>
2024-06-02 16:59:18 +0300

    added styles, active navbar items, routes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import HeaderComponent from '@/components/header/header'
-import './global.scss'
+import '../styles/global.scss'
 
 const inter = Inter({ subsets: ['latin'] })
 
--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,24 +1,41 @@
+'use client'
 import React from 'react'
 import Link from 'next/link'
+import clsx from 'clsx'
 import ContainerComponent from '../container/container'
 import { Route } from '@/constants/common.constants'
+import { usePathname } from 'next/navigation'
 import s from './header.module.scss'
 
 const HeaderComponent = () => {
+  const pathname = usePathname()
+
   return (
     <ContainerComponent>
       <div className={s.header}>
-        <Link href={Route.HOME}>conduit</Link>
-        <ul>
-          <Link className={s.nav_items} href={Route.HOME}>
-            Home
-          </Link>
-          <Link className={s.nav_items} href={Route.SIGN_IN}>
-            Sing in
-          </Link>
-          <Link className={s.nav_items} href={Route.SIGN_UP}>
-            Sign up
-          </Link>
+        <Link className={s.label} href={Route.HOME}>
+          conduit
+        </Link>
+
+        <ul className={s.navbar_container}>
+          <li
+            className={clsx(s.nav_items, pathname === Route.HOME && s.active)}>
+            <Link href={Route.HOME}>Home</Link>
+          </li>
+          <li
+            className={clsx(
+              s.nav_items,
+              pathname === Route.SIGN_IN && s.active
+            )}>
+            <Link href={Route.SIGN_IN}>Sing in</Link>
+          </li>
+          <li
+            className={clsx(
+              s.nav_items,
+              pathname === Route.SIGN_UP && s.active
+            )}>
+            <Link href={Route.SIGN_UP}>Sign up</Link>
+          </li>
         </ul>
       </div>
     </ContainerComponent>
